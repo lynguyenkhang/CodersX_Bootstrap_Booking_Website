44350be6cea96484b7bea46a8de2b7b2b270ecf7
Ly Nguyen Khang <nguyenkhang@Lys-MacBook-Pro.local>
2020-05-31 22:45:05 +0700

    Create 'option navation' 0 -> 1200px in components.html

---
+++ option_nav.js
@@ -0,0 +1,10 @@
+var menuBar = document.getElementById('menu_bar');
+
+
+function showMenuBar() {
+	menuBar.classList.add('show-menu-bar');
+}
+
+function hideMenuBar() {
+	menuBar.classList.remove('show-menu-bar');
+}

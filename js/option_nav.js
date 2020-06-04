94f8eaab4073967086531433589bcd3b843b5001
Ly Nguyen Khang <nguyenkhang@Lys-MacBook-Pro.local>
2020-06-04 09:49:52 +0700

    Fix mistakes in mobile screen

--- js/option_nav.js
+++ js/option_nav.js
@@ -3,8 +3,14 @@ var menuBar = document.getElementById('menu_bar');
 
 function showMenuBar() {
 	menuBar.classList.add('show-menu-bar');
+	setTimeout(function() {
+		menuBar.classList.add('move-menu-bar');
+	},100)
 }
 
 function hideMenuBar() {
-	menuBar.classList.remove('show-menu-bar');
+	menuBar.classList.remove('move-menu-bar');
+	setTimeout(function() {
+		menuBar.classList.remove('show-menu-bar');
+	},760)
 }
